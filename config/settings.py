6941942370427b1af0e90742a9b27b7531d22bbe
Jamoliddin Ibrohimov <78890960+Jamoliddin0968@users.noreply.github.com>
2022-09-11 12:04:48 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-agei0a0!*atxs7z&^$8u!+m#6dq0*vj_n24p_lcfx+vl33*rxk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "media/"
+
+django_heroku.settings(locals())
